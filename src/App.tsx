bb8361bd3775576eb5280c3d04dfb014801acb65
Shubham Rathod <148671273+Rathodshubham07@users.noreply.github.com>
2025-11-12 19:00:44 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProjectZipGenerator from '../project_zip_generator';
+
+const App: React.FC = () => {
+  return <ProjectZipGenerator />;
+};
+
+export default App;
